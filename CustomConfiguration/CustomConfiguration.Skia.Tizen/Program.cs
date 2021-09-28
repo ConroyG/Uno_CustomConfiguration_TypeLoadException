72c5eae96d28e9e7c785712205f60e11bb88a23a
Roycon <roy246810@hotmail.com>
2021-09-28 21:20:53 +1300

    Add project files.

---
+++ CustomConfiguration/CustomConfiguration.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace CustomConfiguration.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new CustomConfiguration.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ CustomConfiguration/CustomConfiguration.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace CustomConfiguration.Skia.Gtk
+{
+}
